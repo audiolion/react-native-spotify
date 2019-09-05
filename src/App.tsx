7ca5319ff33a368e28756fd60c92b5e504f28de2
Ryan Castner <audiolion@users.noreply.github.com>
2019-09-05 01:46:40 -0400

    feat: apollo (#16)
    * build: apollo / graphql deps
    
    * Configure apollo setup
    
    * build: jest-fetch-mock dep

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import { SafeAreaView, StyleSheet, StatusBar } from 'react-native';
 import { RootNav } from './navigation';
+import { ApolloProvider } from '@apollo/react-hooks';
 import { Colors, DEEP_LINKING_SCHEME } from './helpers/constants';
+import { spotifyClient } from './config/apollo';
 
 export const App: React.FC = () => {
   return (
-    <>
+    <ApolloProvider client={spotifyClient}>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView style={styles.appContainer}>
         <RootNav uriPrefix={DEEP_LINKING_SCHEME} />
       </SafeAreaView>
-    </>
+    </ApolloProvider>
   );
 };
 
