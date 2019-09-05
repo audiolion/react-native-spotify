7ed735ec0dbf693d1a7ef461b10fdbc3c600217f
Ryan Castner <castner.rr@gmail.com>
2019-09-05 02:10:57 -0400

    feat: Query component that handles common loading/error states and unauthenticated requests

---
+++ src/components/Query.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { DocumentNode } from 'graphql';
+import { NavigationScreenProp } from 'react-navigation';
+import { OperationVariables } from '@apollo/react-common';
+import { useQuery as useApolloQuery } from '@apollo/react-hooks';
+import { QueryComponentOptions } from '@apollo/react-components';
+import { Loading } from './Loading';
+import { NetworkStatus } from 'apollo-client';
+
+interface Props {
+  query: DocumentNode;
+  navigation: NavigationScreenProp<any>;
+  errorFallback?: React.ReactElement | null;
+  onLoading?: React.ReactElement | null;
+  retry?: () => void;
+}
+
+export function Query<TData = any, TVariables = OperationVariables>({
+  query,
+  children,
+  navigation,
+  onLoading,
+  errorFallback,
+  retry,
+  ...options
+}: Props & Omit<QueryComponentOptions<TData, TVariables>, 'query'>) {
+  const result = useApolloQuery<TData, TVariables>(query, {
+    fetchPolicy: 'cache-and-network',
+    ...options,
+  });
+
+  if (
+    result.loading &&
+    ![NetworkStatus.fetchMore, NetworkStatus.refetch].includes(
+      result.networkStatus
+    )
+  ) {
+    return onLoading || <Loading />;
+  }
+
+  if (result.error) {
+    const { graphQLErrors } = result.error;
+    const isUnauthenticated = graphQLErrors.find(
+      error => !!error.extensions && error.extensions.code === 'UNAUTHENTICATED'
+    );
+    if (isUnauthenticated) {
+      navigation.navigate('Auth');
+      return null;
+    }
+
+    return errorFallback || <Loading error />;
+  }
+
+  return children(result);
+}
