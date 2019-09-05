34154f30d912b10af412af8e03e8a0cfc0e9209a
Ryan Castner <castner.rr@gmail.com>
2019-09-05 00:02:10 -0400

    Implement Linking to spotify authorization url, parsing of callback and pushing to Callback route

--- src/screens/SpotifyLoginScreen.tsx
+++ src/screens/SpotifyLoginScreen.tsx
@@ -1,11 +1,49 @@
 import React from 'react';
+import queryString from 'query-string';
 import { material } from 'react-native-typography';
 import { NavigationScreenProps } from 'react-navigation';
-import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
-import { Colors } from '../helpers/constants';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+  Linking,
+} from 'react-native';
+import { Colors, DEEP_LINKING_SCHEME } from '../helpers/constants';
+
+const scopes = ['user-library-read', 'user-read-private'];
 
 export const SpotifyLoginScreen: React.FC<NavigationScreenProps> = props => {
-  const openSpotifyLogin = () => {};
+  const openSpotifyLogin = () => {
+    const queryParams = {
+      client_id: '3a672567e35644c6b913dfdeea6494bd',
+      redirect_uri: `${DEEP_LINKING_SCHEME}callback`,
+      response_type: 'token',
+      scope: scopes.join(','),
+    };
+
+    Linking.openURL(
+      `https://accounts.spotify.com/authorize?${queryString.stringify(
+        queryParams
+      )}`
+    ).catch(console.error);
+  };
+
+  const handleSpotifyCallback = ({ url }: { url: string }) => {
+    const params = queryString.parse(url);
+    props.navigation.navigate({
+      routeName: 'Callback',
+      params,
+    });
+  };
+
+  React.useEffect(() => {
+    Linking.addEventListener('url', handleSpotifyCallback);
+    return () => {
+      Linking.removeEventListener('url', handleSpotifyCallback);
+    };
+  }, []);
 
   return (
     <View style={styles.center}>
