71556c003098ab0e54be81c50a4de62fd40303d1
Ryan Castner <audiolion@users.noreply.github.com>
2019-09-04 20:44:59 -0400

    chore: remove doc comment [ci skip] (#10)

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/emin93/react-native-template-typescript
- *
- * @format
- */
-
 import React from 'react';
 import { SafeAreaView, StyleSheet, Text, StatusBar } from 'react-native';
 
