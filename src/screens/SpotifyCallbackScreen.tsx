173a917ff3b14572ce869dd88493dfc754718d59
Ryan Castner <castner.rr@gmail.com>
2019-09-05 00:27:20 -0400

    Set token-expires date in AsyncStorage as well

--- src/screens/SpotifyCallbackScreen.tsx
+++ src/screens/SpotifyCallbackScreen.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import addSeconds from 'date-fns/addSeconds';
 import { NavigationScreenProps } from 'react-navigation';
 import AsyncStorage from '@react-native-community/async-storage';
 import { View, ActivityIndicator, StyleSheet } from 'react-native';
@@ -6,10 +7,16 @@ import { Colors } from '../helpers/constants';
 
 export const SpotifyCallbackScreen: React.FC<NavigationScreenProps> = props => {
   React.useEffect(() => {
-    AsyncStorage.setItem(
-      'access-token',
-      props.navigation.getParam('access_token', '')
-    ).then(() => props.navigation.navigate('App'));
+    const accessToken = props.navigation.getParam('access_token', '');
+
+    const now = new Date();
+    const expiresIn = props.navigation.getParam('expires_in', 0);
+    const tokenExpires = addSeconds(now, expiresIn);
+
+    AsyncStorage.multiSet([
+      ['access-token', accessToken],
+      ['token-expires', tokenExpires],
+    ]).then(() => props.navigation.navigate('App'));
   }, []);
 
   return (
